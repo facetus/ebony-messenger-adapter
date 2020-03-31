deb0ec2ec5ddc4781108da403965bf79d0a61d1c
Christos Panagiotakopoulos <chrispanag@gmail.com>
2020-03-31 11:23:31 +0300

    Fix error

--- src/adapter/MessengerUser.ts
+++ src/adapter/MessengerUser.ts
@@ -1,36 +1,45 @@
 import { User } from "ebony-framework";
-import { senderFactory } from './sender';
+import { senderFactory } from "./sender";
 import { UserDataFields } from "./interfaces/messengerAPI";
 
 export default class MessengerUser extends User {
-    private getUserData: (id: string, fields: UserDataFields[]) => Promise<any>;
+  private getUserData: (id: string, fields: UserDataFields[]) => Promise<any>;
 
-    constructor(document: any, pageToken: string) {
-        super(document);
+  constructor(document: any, pageToken: string) {
+    super(document);
 
-        const { getUserData } = senderFactory(pageToken);
-        this.getUserData = getUserData;
+    const { getUserData } = senderFactory(pageToken);
+    this.getUserData = getUserData;
+  }
+
+  async handover() {
+    this.handovered = true;
+    // TODO: Implement
+    return this.save();
+  }
+
+  async dehandover() {
+    this.handovered = false;
+    // TODO: Implement
+    return this.save();
+  }
+
+  async getFacebookData() {
+    console.log("New user");
+    try {
+      const userData = await this.getUserData(this.id, [
+        UserDataFields.firstName,
+        UserDataFields.lastName
+      ]);
+      const { first_name = "", last_name = "", gender = "male" } = userData;
+
+      this.firstName = first_name;
+      this.lastName = last_name;
+      this.gender = gender;
+    } catch (err) {
+      this.firstName = "";
+      this.lastName = "";
+      this.gender = "";
     }
-
-    async handover() {
-        this.handovered = true;
-        // TODO: Implement
-        return this.save();
-    }
-
-    async dehandover() {
-        this.handovered = false;
-        // TODO: Implement
-        return this.save();
-    }
-
-    async getFacebookData() {
-        console.log("New user");
-        const userData = await this.getUserData(this.id, [UserDataFields.firstName, UserDataFields.lastName]);
-        const { first_name = '', last_name = '', gender = "male" } = userData;
-
-        this.firstName = first_name;
-        this.lastName = last_name;
-        this.gender = gender;
-    }
+  }
 }
